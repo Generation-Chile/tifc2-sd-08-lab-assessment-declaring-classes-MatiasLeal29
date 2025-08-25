bd94aa58746c5d6bc3806bcbd54168b7814e55d2
MatiasLeal29 <matias.leal@genstudents.org>
2025-08-25 18:30:27 +0000

    Entrega ML

--- task6.js
+++ task6.js
@@ -1,8 +1,30 @@
-class Player {
-  constructor() {
-    
+export class Player {
+  constructor(name, level = 1) {
+    this.name = name;
+    this.level = level;
   }
 
-  
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`);
+  }
 }
 
+export class Party {
+  constructor() {
+    this.members = [];
+  }
+
+  addPlayer(player) {
+    this.members.push(player);
+    console.log(`${player.name} joined the party!`);
+  }
+
+  removePlayer(playerName) {
+    this.members = this.members.filter(p => p.name !== playerName);
+    console.log(`${playerName} left the party.`);
+  }
+
+  showParty() {
+    console.log("Party members:", this.members.map(p => p.name).join(", "));
+  }
+}
